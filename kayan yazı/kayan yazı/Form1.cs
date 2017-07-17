e6e94134f902376847bbaf026e1838974796c7cc
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:21:15 +0300

    Add files via upload

---
+++ kayan yazı/kayan yazı/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kayan_yazı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+        string isim = " Sinan Acar ";
+        private void timer1_Tick(object sender, EventArgs e) //sinan acar s yazıyo sonra i ılk harf oluyo onu yazıyo sonra n ılk sayı oluyo onu yazıyo
+        {
+            label1.Text = isim.Substring(1) + isim.Substring(0,1);
+            isim = label1.Text;                                                            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
